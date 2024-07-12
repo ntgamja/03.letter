c015b9a0deb95415a833e8ff9292cd2b9bcc3d46
ntgamja <112479847+ntgamja@users.noreply.github.com>
2024-07-13 07:30:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-btn= document.querySelector("button")
+btns= document.querySelectorAll("button")
+console.log(btns)
 
-btn.addEventListener("click",(e)=>{
-    console.log(btn.textContent)
-    b=btn.textContent
-    window.location.href = `./${b}.html`;
-})
+
+btns.forEach(function(item){
+    console.log(item)
+    item.addEventListener("click",(e)=>{
+        console.log(item.textContent)
+        b=item.textContent
+        window.location.href = `./${b}.html`;
+    })
+});
+
 
